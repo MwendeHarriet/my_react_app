fe9d059c7ea23ea48a7ed62d5c87bc491a89cb61
MwendeHarriet <mwendeharriet@gmail.com>
2024-05-24 21:15:28 +0300

    feat: react folders

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} My first React Project</p>
+      <hr />
+    </footer>
+  );
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>My first React Project</h1>
+      <nav>
+        <ul>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>About</li>
+          <li><a href="#"></a>Services</li>
+          <li><a href="#"></a>Contacts</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
